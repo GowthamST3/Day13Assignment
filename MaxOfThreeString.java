754bc8b523542d28b188af7728e5f04e0b21bd3c
GowthamST3 <gowthamgiri3@gmail.com>
2022-05-01 20:23:35 +0530

    Added - To find maximum name by using string type Code

---
+++ MaxOfThreeString.java
@@ -0,0 +1,34 @@
+package com.BridgeLabz_Day13Assignment;
+
+import java.util.Scanner;
+
+public class MaxOfThreeString {
+	public static String maxOfThreeName(String name1,String name2, String name3){
+		String name;
+		Integer num1=(Integer)name1.length();
+		Integer num2=(Integer)name1.length();
+		Integer num3=(Integer)name1.length();
+		if ((num1.compareTo(num2)) > 0 && (num1.compareTo(num3)) > 0){
+			name = name1;
+		} else if (num2.compareTo(num3)>0){
+			name = name2;
+		}else{
+			name = name3;
+		}
+
+		return name;
+	}
+	public static void main(String []args){
+		System.out.println("Find maximum among three Names.");
+		Scanner scanner= new Scanner(System.in);
+		System.out.println("Enter the first, second & third name's....");
+		String name1=scanner.next();
+		String name2=scanner.next();
+		String name3=scanner.next();
+
+		String maxName=maxOfThreeName(name1,name2,name3);
+		System.out.println("The three largest numbers is: "+ maxName);
+	}
+}
+
+
